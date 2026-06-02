32395f25dc7e9a2d2715e2f2321b8d8ccbcf1981
bradgeigermanager-sketch <brad.geiger.manager@gmail.com>
2026-06-02 17:52:28 -0600

    Implement plugin loading mechanism

---
+++ plugins/__init__.py
@@ -0,0 +1,14 @@
+```python
+import pkgutil, importlib
+from .base import BasePlugin
+
+def load_plugins():
+    plugins = []
+    for _, name, _ in pkgutil.iter_modules(__path__):
+        if name != 'base':
+            module = importlib.import_module(f'.{name}', package=__name__)
+            class_name = "".join(p.capitalize() for p in name.split('_')) + "Plugin"
+            plugins.append(getattr(module, class_name)())
+    return plugins
+
+```
